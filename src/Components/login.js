f48ea6a21f1fa88221e91d5f601e44c228d053b8
unknown <mdsakiluzzaman@gmail.com>
2022-03-05 14:27:25 +0600

    styled componenets added

--- src/Components/login.js
+++ src/Components/login.js
@@ -20,8 +20,25 @@ class Login extends Component {
  
 const Container= styled.div`
 padding:0px;
+
 `;
 const Nav=styled.nav`
+max-width:1128px;
+margin:auto;
+padding:12px 0 16px;
+display:flex;
+align-items:center;
+position:relative;
+justify-content:space-between;
+flex-wrap:nowrap;
+
+&>a{
+    width:135px;
+    height:34px;
+    @media(max-width:768px){
+        padding:0 5px;
+    }
+}
 
 `;
 
