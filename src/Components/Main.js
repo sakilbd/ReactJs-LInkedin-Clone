8122a3147b07dd74187bbf3ccaa6f835ad7293f2
unknown <mdsakiluzzaman@gmail.com>
2022-03-17 15:59:19 +0600

    some changes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,6 +4,8 @@ import {BsCardImage} from 'react-icons/bs';
 import {MdOutlineVideoLibrary} from 'react-icons/md';
 import {MdOutlineEventAvailable} from 'react-icons/md';
 import {RiArticleLine} from 'react-icons/ri';
+import {BsThreeDots} from 'react-icons/bs';
+import '../Css/Main.css'
 
 const Main = (props) => {
   return(
@@ -17,24 +19,58 @@ const Main = (props) => {
       <div>
         <button>
           
-             <BsCardImage/>
+             <BsCardImage className="image__icon"/>
         
-          <span>Photo</span>
+          <span >Photo</span>
         </button>
         <button>
-          <MdOutlineVideoLibrary/>
+          <MdOutlineVideoLibrary className="video__icon"/>
           <span>Video</span>
         </button>
         <button>
-          <MdOutlineEventAvailable/>
+          <MdOutlineEventAvailable className="event__icon" />
           <span>Event</span>
         </button>
         <button>
-          <RiArticleLine/>
+          <RiArticleLine className="article__icon"/>
           <span>Write Article</span>
         </button>
       </div>
       </ShareBox>
+      <Article>
+        <SharedActor>
+            <a>
+                <img src="/images/user.svg" alt=""></img>
+                <div>
+                  <span>Title</span>
+                  <span>Info</span>
+                  <span>Date</span>
+                </div>
+            </a>
+            <button>
+                <BsThreeDots/>
+            </button>
+         
+        </SharedActor>
+        <Description>Description</Description>
+        <SharedImg>
+          <a>
+            <img src="/images/shared-image.jpg"></img>
+          </a>
+        </SharedImg>
+        <SocialCounts>
+          <li>
+            <button>
+               <img src="/images/like.png" alt=""></img>
+               <img src='/images/clap.png' alt=""></img>
+                <span>75</span>
+            </button>
+          </li>
+          <li>
+            <a>2 comments</a>
+          </li>
+        </SocialCounts>
+      </Article>
     </Container>
   );
   
@@ -43,7 +79,7 @@ const Main = (props) => {
 const Container=styled.div`
 grid-area:main;
 
-`
+`;
 
 const CommonCard = styled.div`
 text-align:center;
@@ -54,7 +90,7 @@ border-radius:5px;
 position:relative;
 border:none;
 box-shadow:0 0 0 1px rgb(0 0 0 / 15%) , 0 0 0 rgb( 0 0 0 / 20%);
-`
+`;
 
 const ShareBox=styled(CommonCard)`
 display:flex;
@@ -101,11 +137,112 @@ div{
     justify-content:space-around;
     padding-bottom:4px;
     button{
-      display:flex;
-      margin: 5px;
+     span{
+       color:#70b5f9;
+     }
     }
   }
 }
+`;
+
+const Article = styled(CommonCard)`
+padding:0;
+margin:0 0 8px;
+overflow:visible;
+`;
+const SharedActor = styled.div`
+padding-right:40px;
+flex-wrap:nowrap;
+padding:12px 16px 0;
+margin-bottom: 8px;
+align-items:center;
+display:flex;
+a{
+  margin-right:12px;
+  flex-grow:1;
+  overflow:hidden;
+  display:flex;
+  text-decoration:none;
+  img{
+  width:48px;
+  height: 48px;
+}
+  & > div{
+    display:flex;
+    flex-direction:column;
+    flex-grow:1;
+    flex-basis:0;
+    margin-left:8px;
+    overflow:hidden;
+    span{
+      text-align:left;
+      &:first-child{
+        font-size:14px;
+        font-weight:700;
+        color:black;
+        color:rgba(0,0,0,1);
+      }
+      &:nth-child(n+1){
+        font-size:12px;
+        color:rgba( 0,0,0,.6);
+
+      }
+    }
+  }
+}
+
+button{
+  position:absolute;
+  border:none;
+  background:transparent;
+  right:12px;
+  top:0;
+  outline:none;
+ 
+
+}
 `
 
+const Description = styled.div`
+padding:0 16px;
+overflow:hidden;
+color:rgba(0,0,0,.9);
+font-size:14px;
+text-align:left;
+`;
+
+const SharedImg = styled.div`
+margin-top:8px;
+
+display: block;
+position:relative;
+background-color:#f9fafb;
+img{
+  width:100%;
+}
+`;
+
+const SocialCounts = styled.ul`
+line-height:1.3;
+display:flex;
+align-items:flex-start;
+overflow:auto;
+margin: 0 16px;
+padding: 8px 0;
+border-bottom: 1px solid #e9e5df;
+list-style-type: style none;
+li{
+  margin-right:5px;
+  font-size:12px;
+    button{
+      display:flex;
+      img:first-child{
+        height:20px;
+      }
+      img:nth-child(2){
+        height:32px;;
+      }
+    }
+}
+`
 export default Main;
